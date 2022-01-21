3e4df6253d696d1dfa025beaadc932ff5c04fd33
[Pratheek Gowda] <pratheekgowdadj@gmail.com>
2022-01-21 19:32:35 +0530

    initial commit

---
+++ PhotoEditing-app-main/src/SidebarItem.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default function SidebarItem({name,active,handelClick}) {
+    return (
+        
+            <button 
+            className={`sidebar-item ${active ? 'active':''}`}
+            onClick={handelClick}
+            >{name}</button>
+        
+    )
+}
